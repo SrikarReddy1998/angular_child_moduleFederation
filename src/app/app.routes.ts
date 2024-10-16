f5c9f7ae5a45850590e7679f99219791a6f5033a
srikarreddy1998 <master.srikarreddy@gmail.com>
2024-10-16 23:44:13 +0530

    http package

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { AppComponent } from './app.component';
+// import { ChildCustomCComponent } from './components/child-custom-c/child-custom-c.component';
+
+// const routes: Routes = [
+//   {
+//     path: 'remote',
+//     component: AppComponent,
+//     pathMatch: 'full',
+//   },
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forChild(routes)],
+//   exports: [RouterModule],
+// })
+// export class AppRoutingModule {}
+
+// if module is exported and not a standalone component
+
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+// import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app.routes';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// @NgModule({
+//   declarations: [AppComponent],
+//   imports: [CommonModule, BrowserModule, AppRoutingModule],
+//   bootstrap: [AppComponent],
+// })
+// export class ChildMModule {}
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,5 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+// if module is exported and not a standalone component
+// platformBrowserDynamic()
+//   .bootstrapModule(ChildMModule)
+//   .catch((err) => console.error(err));
